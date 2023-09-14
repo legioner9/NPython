2c4c5b828b517e5c833d2bbaf80a53bfa52ad439
[alt]legioner9 <legioner9@inbox.ru>
2023-09-14 21:43:09 +0700

    <> ##

---
+++ sfu/pract_task/1/1.py
@@ -0,0 +1,15 @@
+import sys
+
+list_args = []
+# iterating till the range
+for i in range(0, 2):
+    item = int(input())
+    # adding the element
+    list_args.append(item)
+
+print(list_args)
+
+# def current_power(current, voltage):
+#     return current**2*voltage
+
+# current_power 

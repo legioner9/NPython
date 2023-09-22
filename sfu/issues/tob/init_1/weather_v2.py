21122df0c5aef39f85b96399df29f4baf69bab94
[alt]legioner9 <legioner9@inbox.ru>
2023-09-22 22:49:57 +0700

    <> #4

--- sfu/issues/tob/init_1/weather_v2.py
+++ sfu/issues/tob/init_1/weather_v2.py
@@ -27,8 +27,6 @@ mess = "temperature: " + f"{temperature}" + " windspeed: " + f"{windspeed}"
 
 print(mess)
 
-list_text_message = [['hello', 'И тебе hello'], ['33', '2']]
-
 
 # os.system('date +"Today is: %A %d %B"')
 # os.system('curl https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true')
@@ -42,39 +40,62 @@ bot = telebot.TeleBot(TKN)
 # Тут работаем с командой start
 
 
-def bot_prn_message(arg_message, arg_str):
-    bot.send_message(arg_message.chat.id, arg_str)
-
-
 @bot.message_handler(commands=['1'])
 def welcome_start(message):
-    bot_prn_message(message, '1')
-
-
-@bot.message_handler(commands=['start'])
-def welcome_start(message):
-    bot_prn_message(message, 'Приветствую тебя user')
-    # bot.send_message(message.chat.id, 'Приветствую тебя user')
-
-
-@bot.message_handler(commands=['get_weather', 'weather', 'pogoda'])
-def get_weather(message):
-    # bot_prn_message(message, text=f"{mess}")
-    bot.send_message(message.chat.id, f"{mess}")
-
-
-@bot.message_handler(commands=['help'])
-def welcome_help(message):
-    bot_prn_message(message, 'Чем я могу тебе помочь')
-
-# 'commands' before 'content_types ?
+    bot.send_message(message.chat.id, '1')
 
 
 @bot.message_handler(content_types=["text"])
 def text(message):
-    for item in list_text_message:
-        if message.text == item[0]:
-            bot_prn_message(message, item[1])
+    if message.text == '33':
+        bot.send_message(message.chat.id, '2')
 
+
+@bot.message_handler(commands=['start'])
+def welcome_start(message):
+    bot.send_message(message.chat.id, 'Приветствую тебя user')
+
+
+
+@bot.message_handler(commands=['get_weather', 'weather', 'pogoda'])
+def get_weather(message):
+    bot.send_message(message.chat.id, text=f"{mess}")
+
+
+
+
+@bot.message_handler(commands=['help'])
+def welcome_help(message):
+    bot.send_message(message.chat.id, 'Чем я могу тебе помочь')
+
+
+
+
+@bot.message_handler(content_types=["text"])
+def text(message):
+    if message.text == 'hello':
+        bot.send_message(message.chat.id, 'И тебе hello')
+
+pprint(bot.message_handlers)
+
+# [{'filters': {'commands': ['1'], 'content_types': ['text']},
+#   'function': <function welcome_start at 0x7ff88f01ddc0>,
+#   'pass_bot': False},
+#  {'filters': {'content_types': ['text']},
+#   'function': <function text at 0x7ff88f01de50>,
+#   'pass_bot': False},
+#  {'filters': {'commands': ['start'], 'content_types': ['text']},
+#   'function': <function welcome_start at 0x7ff88f01dee0>,
+#   'pass_bot': False},
+#  {'filters': {'commands': ['get_weather', 'weather', 'pogoda'],
+#               'content_types': ['text']},
+#   'function': <function get_weather at 0x7ff88f01df70>,
+#   'pass_bot': False},
+#  {'filters': {'commands': ['help'], 'content_types': ['text']},
+#   'function': <function welcome_help at 0x7ff88f027040>,
+#   'pass_bot': False},
+#  {'filters': {'content_types': ['text']},
+#   'function': <function text at 0x7ff88f0270d0>,
+#   'pass_bot': False}]
 
 bot.polling(none_stop=True)

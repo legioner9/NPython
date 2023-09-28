0ada7908faddadcb189e7fecde1b1dcb9da17e0e
[alt]legioner9 <legioner9@inbox.ru>
2023-09-28 17:58:54 +0700

    <> weather #2

--- sfu/issues/tob/init_1/weather_v4.py
+++ sfu/issues/tob/init_1/weather_v4.py
@@ -1,6 +1,6 @@
 # pip install pyTelegramBotAPI
 # bot: https://t.me/elrwegBot
-# send message in bot /help
+# send message in bot /weather or /get_weather or /pogoda
 
 import telebot
 from telebot import types
@@ -14,7 +14,7 @@ import subprocess
 from pprint import pprint
 
 output2 = subprocess.check_output(
-    'echo $(curl https://api.open-meteo.com/v1/forecast\?latitude=56.00\&longitude=92.52\&current_weather=true)', shell=True)
+    'echo $(curl https://api.open-meteo.com/v1/forecast\?latitude=52.52\&longitude=13.41\&current_weather=true)', shell=True)
 
 print("output2 :", output2)
 
@@ -26,8 +26,7 @@ print("time: ", cw["time"])
 
 temperature = cw["temperature"]
 windspeed = cw["windspeed"]
-time = cw["time"]
-mess = f"Погода в Красноярске:\n     время: (по Гринвичу +7 Красноярск) {time}\n     температура воздуха: {temperature}\n     ветер: {windspeed}"
+mess = "temperature: " + f"{temperature}" + " windspeed: " + f"{windspeed}"
 
 print(mess)
 
@@ -50,14 +49,19 @@ def bot_prn_message(arg_message, arg_str):
     bot.send_message(arg_message.chat.id, arg_str)
 
 
+@bot.message_handler(commands=['1'])
+def welcome_start(message):
+    bot_prn_message(message, '1')
+
+
 @bot.message_handler(commands=['start'])  # создаем команду
 def start(message):
     markup = types.InlineKeyboardMarkup()
     button0 = types.InlineKeyboardButton(
-        "Сайт gismeteo.ru Krasnoyarsk 10 days", url='https://www.gismeteo.ru/weather-krasnoyarsk-4674/10-days/')
+        "Сайт gismeteo Divnogorsk 10 days", url='https://www.gismeteo.ru/weather-divnogorsk-11843/10-days/')
     markup.add(button0)
     button1 = types.InlineKeyboardButton(
-        "Сайт yandex.ru Krasnoyarsk 10 days", url='https://yandex.ru/pogoda/ru-RU/details?lat=56.01056671&lon=92.85256958&lang=ru&via=ms')
+        "Сайт yandex Divnogorsk 10 days", url='https://yandex.ru/pogoda/divnogorsk')
     markup.add(button1)
     bot.send_message(message.chat.id, "Привет, {0.first_name}! Нажми на кнопку и перейди на сайт в браузере)".format(
         message.from_user), reply_markup=markup)
@@ -72,13 +76,11 @@ def get_weather(message):
 
 @bot.message_handler(commands=['help'])
 def welcome_help(message):
-    bot_prn_message(message, 'данный бот создан в учебных целях,\nзапрашивает погоду с сайта api.open-meteo.com для вывода ответа в текстовом режиме в Красноярске по команде /pogoda,\nотображает кнопку перехода на сайт www.gismeteo.ru и кнопку перехода на сайт yandex.ru  по команде /start')
-
-
+    bot_prn_message(message, 'данный бот создан в учебных целях,\nзапрашивает погоду с сайта api.open-meteo.com для вывода ответа в текстовом режиме в Красноярске по команде /pogoda,\nотображает кнопки перехода на сайт www.gismeteo.ru по команде /start')
+    
 @bot.message_handler(commands=['menu'])
 def welcome_menu(message):
-    bot_prn_message(
-        message, '/menu - вывод всех команд\n/start - вывод интерактивных кнопок\n/pogoda - вывод погоды\n/help - вывод подсказки')
+    bot_prn_message(message, '/menu - вывод всех команд\n/start - вывод интерактивных кнопок\n/help - вывод подсказки')
 
 # 'commands' before 'content_types ?
 
